7aeed0a0c2ee3fa34a31d6573b2030053f15cc6d
dev machine <generationiprogramming@gmail.com>
2025-05-20 15:44:49 +0530

    movie poster page, footer links

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,17 @@ export default defineConfig({
       input: {
         main: 'index.html',
         service: 'service.html',
-        team:'team.html',
-        contact:'contactus.html',
-        about:'aboutus.html',
-        work:'work.html',
-        'work-details':'work-details.html',
-        'work1':'work-1.html',
-        'movie-posters':'movie-poster.html',
+        team: 'team.html',
+        contact: 'contactus.html',
+        about: 'aboutus.html',
+        work: 'work.html',
+        'movie-posters': 'movie-poster.html',
+        'film-posters': 'film-posters.html',
+        events: 'events.html',
+        'title-animations': 'title-animations.html',
+        branding: 'branding.html',
+        products: 'products.html',
+        advertising: 'advertising.html',
       }
     }
   }
